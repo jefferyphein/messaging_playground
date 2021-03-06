8d1425f316723a434fd2eb868ff5a8530389fb08
Erik Stokes <stokes.erik@gmail.com>
2021-03-06 13:50:35 -0500

    Clean up linter warnings

--- rabbitmq/python/work_distribution/work_distribution.py
+++ rabbitmq/python/work_distribution/work_distribution.py
@@ -1,58 +1,61 @@
+"Work pool RabbitMQ consumer"
+
 import concurrent.futures
-import queue
-import functools
-import logging
 import ssl
 import time
 
 import pika
 
-RABBITMQ_HOST='localhost'
+RABBITMQ_HOST = 'localhost'
+
 
 def work_function(body):
-    print("Starting work on %s"%body)
+    "Do work. Takes a little while."
+    print("Starting work on %s" % body)
     time.sleep(5)
-    print("Work on %s finished"%body)
+    print("Work on %s finished" % body)
+
 
 class Consumer:
+    "RabbitMQ consumer"
     def __init__(self, params, prefetch, on_message_callback):
-        self.connection = pika.SelectConnection(params, on_open_callback=self.open_channel,
-                                                on_open_error_callback=self.open_error)
+        self.connection = pika.SelectConnection(params,
+                                                on_open_callback=self.open_channel)
         self.on_message_callback = on_message_callback
         self.prefetch = prefetch
         self.channel = None
         self.queue = None
 
-    def open_error(self, connection, exc):
-        print("connection failed to open with exception %s"%exc)
-
     def start_consumer(self):
-        self.connection.ioloop.start() #blocks
+        "Block and start consuming messages"
+        self.connection.ioloop.start()  # blocks
 
-    def setup_queue(self, channel):
+    def _setup_queue(self, channel):
         print("Declaring queue")
 
         def cb(method):
             self.queue = method.method.queue
             print("Starting consumer")
             self.channel.basic_qos(prefetch_count=self.prefetch)
-            self.channel.basic_consume(self.queue, on_message_callback=self.on_message_callback)
+            self.channel.basic_consume(self.queue,
+                                       on_message_callback=self.on_message_callback)
 
         channel.queue_declare('', exclusive=True, auto_delete=True, callback=cb)
 
     def open_channel(self, connection):
+        "Open a channel on the connection, and start a consumer on it."
         print("Opening channel")
-        self.channel = self.connection.channel(on_open_callback=self.setup_queue)
+        self.channel = self.connection.channel(on_open_callback=self._setup_queue)
 
 
-if __name__ == '__main__':
+def main():
     # logging.basicConfig(level=logging.INFO)
     ctx = ssl.create_default_context(cafile="tls-gen/basic/result/ca_certificate.pem")
     ctx.load_cert_chain("tls-gen/basic/result/client_certificate.pem",
                         keyfile="tls-gen/basic/result/client_key.pem",
                         password="bunnies")
     ssl_opts = pika.SSLOptions(ctx, RABBITMQ_HOST)
-    creds =pika.credentials.ExternalCredentials()
+    creds = pika.credentials.ExternalCredentials()
     # creds = pika.PlainCredentials(username='guest', password='guest')
     params = pika.ConnectionParameters(RABBITMQ_HOST,
                                        virtual_host='/',
@@ -73,4 +76,9 @@ if __name__ == '__main__':
         return
 
     consumer = Consumer(params, prefetch=4, on_message_callback=process_message)
-    consumer.start_consumer() # blocks
+    consumer.start_consumer()  # blocks
+    return 0
+
+
+if __name__ == '__main__':
+    main()
