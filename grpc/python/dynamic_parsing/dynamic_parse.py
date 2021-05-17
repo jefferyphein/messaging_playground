983f18feeb287a9390dba0f3369a4d5a37ef0c5b
Erik Stokes <stokes.erik@gmail.com>
2021-05-17 14:58:19 -0400

    Same thing, but with functions and stuff

--- grpc/python/dynamic_parsing/dynamic_parse.py
+++ grpc/python/dynamic_parsing/dynamic_parse.py
@@ -11,32 +11,55 @@ import google.protobuf.reflection
 from google.protobuf import message as _message
 
 
-parser = argparse.ArgumentParser(
-    description="Read the given protobuf descriptor and parse binary data on stdin"
-)
-parser.add_argument("descriptor")
-parser.add_argument('message_type')
+def load_descriptor(pool, desc_file):
+    """Load the binary proto data from a file into a descriptor pool.
 
-args = parser.parse_args()
+    This loads data as generatred by e.g.
+    `protoc -o hello.desc protos/hello.proto`
 
-pool = google.protobuf.descriptor_pool.DescriptorPool()
+    """
+    with open(desc_file, 'rb') as infile:
+        file_desc = google.protobuf.descriptor_pb2.FileDescriptorSet.FromString(infile.read())
 
-# Load the binary proto data from a file. This was generatred with
-# `protoc -o hello.desc protos/hello.proto`
-with open(args.descriptor, 'rb') as infile:
-    file_desc = google.protobuf.descriptor_pb2.FileDescriptorSet.FromString(infile.read())
+    for proto in file_desc.file:
+        pool.Add(proto)
+    return
 
-for proto in file_desc.file:
-    pool.Add(proto)
 
-# Now load the message type from the pool
-msg_desc = pool.FindMessageTypeByName(args.message_type)
-Message = google.protobuf.reflection.GeneratedProtocolMessageType(
-    args.message_type.split('.')[-1],
-    (_message.Message,),
-    {'DESCRIPTOR': msg_desc}
-)
+def get_message_class(pool, message_type):
+    """Get the message class from the pool
 
-# Read data from stdin
-data = Message.FromString(sys.stdin.buffer.read())
-print(data)
+    message type is of the form 'package.Message'
+
+    """
+    msg_desc = pool.FindMessageTypeByName(message_type)
+    Message = google.protobuf.reflection.GeneratedProtocolMessageType(
+        message_type.split('.')[-1],
+        (_message.Message,),
+        {'DESCRIPTOR': msg_desc}
+    )
+    return Message
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Read the given protobuf descriptor and parse binary data on stdin"
+    )
+    parser.add_argument("descriptor")
+    parser.add_argument('message_type')
+
+    args = parser.parse_args()
+
+    pool = google.protobuf.descriptor_pool.DescriptorPool()
+    load_descriptor(pool, args.descriptor)
+
+    Message = get_message_class(pool, args.message_type)
+
+    # Read data from stdin
+    data = Message.FromString(sys.stdin.buffer.read())
+    print(data)
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
