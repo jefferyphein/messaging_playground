625f40b59fedc730b81b564d0052ffe76935121c
Erik Stokes <stokes.erik@gmail.com>
2021-05-19 15:01:16 -0400

    Use caching pool with dynamic parser toy

--- grpc/python/dynamic_parsing/dynamic_parse.py
+++ grpc/python/dynamic_parsing/dynamic_parse.py
@@ -10,35 +10,17 @@ import google.protobuf.descriptor_pool
 import google.protobuf.reflection
 from google.protobuf import message as _message
 
+import fetch
 
-def load_descriptor(pool, desc_file):
-    """Load the binary proto data from a file into a descriptor pool.
-
-    This loads data as generatred by e.g.
-    `protoc -o hello.desc protos/hello.proto`
-
-    """
-    with open(desc_file, 'rb') as infile:
-        file_desc = google.protobuf.descriptor_pb2.FileDescriptorSet.FromString(infile.read())
-
-    for proto in file_desc.file:
-        pool.Add(proto)
-    return
-
-
-def get_message_class(pool, message_type):
-    """Get the message class from the pool
-
-    message type is of the form 'package.Message'
-
-    """
-    msg_desc = pool.FindMessageTypeByName(message_type)
-    Message = google.protobuf.reflection.GeneratedProtocolMessageType(
-        message_type.split('.')[-1],
-        (_message.Message,),
-        {'DESCRIPTOR': msg_desc}
-    )
-    return Message
+def parse_content_type(content_type):
+    parts = [p.strip() for p in content_type.split(';')]
+    content_type = parts[0]
+    rest = parts[:1]
+    args = {}
+    for a in rest:
+        key,val = a.split('=')
+        args[key.strip()] = val.strip()
+    return content_type, args
 
 
 def main():
@@ -50,10 +32,8 @@ def main():
 
     args = parser.parse_args()
 
-    pool = google.protobuf.descriptor_pool.DescriptorPool()
-    load_descriptor(pool, args.descriptor)
-
-    Message = get_message_class(pool, args.message_type)
+    pool = fetch.DescriptorFetcher(5*60)
+    Message = pool.fetch(args.descriptor, args.message_type)
 
     # Read data from stdin
     data = Message.FromString(sys.stdin.buffer.read())
