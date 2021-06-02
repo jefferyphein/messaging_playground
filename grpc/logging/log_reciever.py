7fc0766839d2f7c732482e3fa00cea6cd8c2abd8
Erik Stokes <stokes.erik@gmail.com>
2021-06-02 14:22:36 -0400

    Add logging daemon toy

---
+++ grpc/logging/log_reciever.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from concurrent.futures import ThreadPoolExecutor
+import asyncio
+
+import grpc
+
+import message_pb2_grpc
+import message_pb2
+
+
+class LogReciever(message_pb2_grpc.LoggerServicer):
+    def WriteLog(self, request, context):
+        print(request)
+        return message_pb2.google_dot_protobuf_dot_empty__pb2.Empty()
+
+async def run():
+    port = 7106407
+    server = grpc.aio.server(ThreadPoolExecutor())
+    server.add_insecure_port("[::]:%s"%port)
+    message_pb2_grpc.add_LoggerServicer_to_server(LogReciever(), server)
+    await server.start()
+    await server.wait_for_termination()
+
+if __name__ == '__main__':
+    asyncio.run(run())
---
+++ grpc/logging/log_writer.py
@@ -0,0 +1,38 @@
+import datetime
+import threading
+import os
+from  enum import Enum
+
+import grpc
+
+import message_pb2_grpc
+import message_pb2
+
+class LogLevel(Enum):
+    DEBUG = 0
+    INFO = 1
+    WARNING = 2
+    ERROR = 3
+    CRITICAL = 4
+
+
+class Logger:
+    def __init__(self):
+        self.channel = grpc.insecure_channel('localhost:7106407')
+        self.stub = message_pb2_grpc.LoggerStub(self.channel)
+
+    def write_log(self, level, message):
+
+        req = message_pb2.LogMessage(
+            level=level.value,
+            utc_timestamp = int(datetime.datetime.utcnow().timestamp()),
+            msg=message,
+            pid = os.getpid(),
+            thread=threading.current_thread().ident,
+        )
+        response = self.stub.WriteLog(req)
+        return
+
+if __name__ == '__main__':
+    logger = Logger()
+    logger.write_log(LogLevel.INFO, "hello")
